d496d632e427791d086a192a165dcdf20e639e33
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2020-04-12 09:06:18 +0300

    AggregateID generator added

--- src/Swisschain.Extensions.Idempotency.EfCore/ModelBuilderExtensions.cs
+++ src/Swisschain.Extensions.Idempotency.EfCore/ModelBuilderExtensions.cs
@@ -4,11 +4,16 @@ namespace Swisschain.Extensions.Idempotency.EfCore
 {
     public static class ModelBuilderExtensions
     {
-        public static ModelBuilder BuildOutbox(this ModelBuilder modelBuilder, string tableName = default)
+        public static ModelBuilder BuildOutbox(this ModelBuilder modelBuilder, string tableName = default, long startAggregateIdFrom = 0L)
         {
             modelBuilder.Entity<OutboxEntity>()
                 .ToTable(tableName ?? "outbox")
                 .HasKey(x => x.RequestId);
+
+            modelBuilder.Entity<OutboxEntity>()
+                .Property(x => x.AggregateId)
+                .HasIdentityOptions(startValue: startAggregateIdFrom)
+                .ValueGeneratedOnAdd();
 
             return modelBuilder;
         }
