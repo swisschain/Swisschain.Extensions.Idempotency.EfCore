84e92fe5dbc2069659edf84e998b5e6bf4e33124
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2020-08-13 13:00:03 +0300

    Tests refactored

--- tests/Tests/Sdk/TestsFixture.cs
+++ tests/Tests/Sdk/TestsFixture.cs
@@ -1,12 +1,77 @@
-﻿using Swisschain.Extensions.Testing;
+﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging.Abstractions;
+using Swisschain.Extensions.Idempotency;
+using Swisschain.Extensions.Idempotency.EfCore;
+using Swisschain.Extensions.Testing;
+using Tests.Sdk.DbContexts;
 
 namespace Tests.Sdk
 {
     public class TestsFixture : PostgresFixture
     {
+        private readonly ServiceProvider _serviceProvider;
+
         public TestsFixture() :
             base("idempotency-tests-pg")
         {
+            OutboxDispatcher = new InMemoryOutboxDispatcher();
+
+            var services = new ServiceCollection();
+            
+            services.AddLogging();
+            services.AddIdempotency<TestUnitOfWork>(x =>
+            {
+                x.PersistWithEfCore(s => CreateDbContext());
+                x.Services.AddSingleton<IOutboxDispatcher>(OutboxDispatcher);
+            });
+
+            _serviceProvider = services.BuildServiceProvider();
+
+            UnitOfWorkManager = _serviceProvider.GetRequiredService<IUnitOfWorkManager<TestUnitOfWork>>();
+        }
+
+        public InMemoryOutboxDispatcher OutboxDispatcher { get; }
+        public IUnitOfWorkManager<TestUnitOfWork> UnitOfWorkManager { get; }
+        public TestEntitiesRepository TestEntitiesRepository { get; private set; }
+        public IOutboxReadRepository OutboxReadRepository { get; private set; }
+        public IIdGeneratorRepository IdGeneratorRepository { get; private set; }
+
+        protected override async Task InitializeAsync()
+        {
+            await CreateTestDb();
+
+            var dbContext = CreateDbContext();
+
+            await dbContext.Database.MigrateAsync();
+
+            TestEntitiesRepository = new TestEntitiesRepository(dbContext);
+            OutboxReadRepository = new OutboxReadRepository<TestDbContextWithDefaultOptions>(
+                NullLogger<OutboxReadRepository<TestDbContextWithDefaultOptions>>.Instance,
+                () => dbContext);
+            IdGeneratorRepository = new IdGeneratorRepository<TestDbContextWithDefaultOptions>(() => dbContext);
+        }
+
+        protected override async Task DisposeAsync()
+        {
+            await DropTestDb();
+
+            await _serviceProvider.DisposeAsync();
+        }
+
+        private TestDbContextWithDefaultOptions CreateDbContext()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<TestDbContextWithDefaultOptions>();
+
+            optionsBuilder
+                .UseNpgsql(GetConnectionString(),
+                    builder =>
+                        builder.MigrationsHistoryTable(
+                            "__Migrations",
+                            TestDbContextWithDefaultOptions.Schema));
+
+            return new TestDbContextWithDefaultOptions(optionsBuilder.Options);
         }
     }
 }
--- tests/Tests/UnitOfWorkTests.cs
+++ tests/Tests/UnitOfWorkTests.cs
@@ -1,21 +1,15 @@
 ﻿using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using Shouldly;
 using Swisschain.Extensions.Idempotency;
-using Swisschain.Extensions.Idempotency.EfCore;
-using Swisschain.Extensions.Testing;
 using Tests.Sdk;
-using Tests.Sdk.DbContexts;
 using Xunit;
 
 namespace Tests
 {
-    public class UnitOfWorkTests : IClassFixture<TestsFixture>, IAsyncLifetime
-
+    public class UnitOfWorkTests : IClassFixture<TestsFixture>
     {
-        private readonly PostgresFixture _fixture;
+        private readonly TestsFixture _fixture;
 
         public UnitOfWorkTests(TestsFixture fixture)
         {
@@ -25,28 +19,15 @@ namespace Tests
         [Fact]
         public async Task TestPositiveCase()
         {
-            var services = new ServiceCollection();
-            var outboxDispatcher = new InMemoryOutboxDispatcher();
+            // Arrange
+
             var domainEvent = new TestDomainEvent {Id = 1};
             var domainCommand = new TestDomainCommand {Id = 2};
             var domainResponse = new TestDomainResponse {Id = 3};
 
-            services.AddLogging();
-            services.AddIdempotency<TestUnitOfWork>(x =>
-            {
-                x.PersistWithEfCore(s => CreateDbContext());
-                x.Services.AddSingleton<IOutboxDispatcher>(outboxDispatcher);
-            });
+            // Act
 
-            await using var serviceProvider = services.BuildServiceProvider();
-
-            var dbContext = CreateDbContext();
-
-            await dbContext.Database.MigrateAsync();
-
-            var unitOfWorkManager = serviceProvider.GetRequiredService<IUnitOfWorkManager<TestUnitOfWork>>();
-
-            await using var unitOfWork = await unitOfWorkManager.Begin("a");
+            await using var unitOfWork = await _fixture.UnitOfWorkManager.Begin("a");
 
             if (!unitOfWork.Outbox.IsClosed)
             {
@@ -61,46 +42,22 @@ namespace Tests
 
             await unitOfWork.EnsureOutboxDispatched();
 
-            var testEntitiesRepository = new TestEntitiesRepository(dbContext);
+            // Assert
 
-            var readName = await testEntitiesRepository.GetNameOrDefault(1);
+            var readName = await _fixture.TestEntitiesRepository.GetNameOrDefault(1);
 
             readName.ShouldBe("one");
 
-            outboxDispatcher.PublishedEvents.OfType<TestDomainEvent>().ShouldHaveSingleItem();
-            outboxDispatcher.PublishedEvents.OfType<TestDomainEvent>().Single().Id.ShouldBe(1);
+            _fixture.OutboxDispatcher.PublishedEvents.OfType<TestDomainEvent>().ShouldHaveSingleItem();
+            _fixture.OutboxDispatcher.PublishedEvents.OfType<TestDomainEvent>().Single().Id.ShouldBe(1);
 
-            outboxDispatcher.SentCommands.OfType<TestDomainCommand>().ShouldHaveSingleItem();
-            outboxDispatcher.SentCommands.OfType<TestDomainCommand>().Single().Id.ShouldBe(2);
+            _fixture.OutboxDispatcher.SentCommands.OfType<TestDomainCommand>().ShouldHaveSingleItem();
+            _fixture.OutboxDispatcher.SentCommands.OfType<TestDomainCommand>().Single().Id.ShouldBe(2);
 
             unitOfWork.Outbox.GetResponse<TestDomainResponse>().ShouldNotBeNull();
             unitOfWork.Outbox.GetResponse<TestDomainResponse>().Id.ShouldBe(3);
         }
-
-        public Task InitializeAsync()
-        {
-            return _fixture.CreateTestDb();
-        }
-
-        Task IAsyncLifetime.DisposeAsync()
-        {
-            return _fixture.DropTestDb();
-        }
-
-        private TestDbContextWithDefaultOptions CreateDbContext()
-        {
-            var optionsBuilder = new DbContextOptionsBuilder<TestDbContextWithDefaultOptions>();
-
-            optionsBuilder
-                .UseNpgsql(_fixture.GetConnectionString(),
-                    builder =>
-                        builder.MigrationsHistoryTable(
-                            "__Migrations",
-                            TestDbContextWithDefaultOptions.Schema));
-
-            return new TestDbContextWithDefaultOptions(optionsBuilder.Options);
-        }
-
+        
         private class TestDomainEvent
         {
             public long Id { get; set; }
