76d24755917b4fdedfa24b39c3c5a0457cfe0f59
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2021-04-28 13:47:23 +0300

    UnitOfWork instantiation via service provider

--- src/Swisschain.Extensions.Idempotency.EfCore/UnitOfWorkFactory.cs
+++ src/Swisschain.Extensions.Idempotency.EfCore/UnitOfWorkFactory.cs
@@ -5,22 +5,26 @@ using Microsoft.EntityFrameworkCore;
 namespace Swisschain.Extensions.Idempotency.EfCore
 {
     internal sealed class UnitOfWorkFactory<TUnitOfWork, TDbContext> : IUnitOfWorkFactory<TUnitOfWork>
-        where TUnitOfWork : UnitOfWorkBase<TDbContext>, new()
+        where TUnitOfWork : UnitOfWorkBase<TDbContext>
         where TDbContext : DbContext, IDbContextWithOutbox, IDbContextWithIdGenerator 
     {
         private readonly IOutboxDispatcher _defaultOutboxDispatcher;
         private readonly Func<TDbContext> _dbContextFactory;
+        private readonly Func<TUnitOfWork> _unitOfWorkFactory;
 
-        public UnitOfWorkFactory(IOutboxDispatcher defaultOutboxDispatcher, Func<TDbContext> dbContextFactory)
+        public UnitOfWorkFactory(IOutboxDispatcher defaultOutboxDispatcher, 
+            Func<TDbContext> dbContextFactory,
+            Func<TUnitOfWork> unitOfWorkFactory)
         {
             _defaultOutboxDispatcher = defaultOutboxDispatcher;
             _dbContextFactory = dbContextFactory;
+            _unitOfWorkFactory = unitOfWorkFactory;
         }
 
         public async Task<TUnitOfWork> Create(Outbox outbox)
         {
             var dbContext = _dbContextFactory.Invoke();
-            var unitOfWork = new TUnitOfWork();
+            var unitOfWork = _unitOfWorkFactory.Invoke();
 
             await unitOfWork.Init(_defaultOutboxDispatcher, dbContext, outbox);
 
@@ -30,7 +34,7 @@ namespace Swisschain.Extensions.Idempotency.EfCore
         public async Task<TUnitOfWork> Create()
         {
             var dbContext = _dbContextFactory.Invoke();
-            var unitOfWork = new TUnitOfWork();
+            var unitOfWork = _unitOfWorkFactory.Invoke();
 
             await unitOfWork.Init(dbContext);
 
