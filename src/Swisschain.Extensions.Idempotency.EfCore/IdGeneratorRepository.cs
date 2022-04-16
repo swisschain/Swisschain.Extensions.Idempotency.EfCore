891a2bb790607f447211da072a74774b06a024eb
Konstantin Ryazantsev <kj.ryazancev@gmail.com>
2022-04-16 19:22:49 +0300

    Supported custom naming conventions for ID generator

--- src/Swisschain.Extensions.Idempotency.EfCore/IdGeneratorRepository.cs
+++ src/Swisschain.Extensions.Idempotency.EfCore/IdGeneratorRepository.cs
@@ -2,12 +2,16 @@
 using System.Data;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace Swisschain.Extensions.Idempotency.EfCore
 {
     internal class IdGeneratorRepository<TDbContext> : IIdGeneratorRepository
         where TDbContext : DbContext, IDbContextWithIdGenerator
     {
+        private static readonly object _dbSchemaCachelock = new object();
+        private static DbSchema _dbSchemaCache;
+
         private readonly Func<TDbContext> _dbContextFactory;
 
         public IdGeneratorRepository(Func<TDbContext> dbContextFactory)
@@ -22,25 +26,24 @@ namespace Swisschain.Extensions.Idempotency.EfCore
             var defaultSchema = context.Model.GetDefaultSchema();
             var nextValue = await GetNextValue(defaultSchema, generatorName, context);
 
-            // TODO: Cache it
-            var tableName = context.Model.FindEntityType(typeof(IdGeneratorEntity)).GetTableName();
+            var dbSchema = GetDbSchema(context);
             var query = $@"
                 with new_value as 
                 (
-                    insert into {defaultSchema}.{tableName} (""IdempotencyId"", ""Value"") values (@idempotencyId, @nextValue)
-                    on conflict(""IdempotencyId"") do nothing
-                    returning ""Value""
+                    insert into {defaultSchema}.{dbSchema.TableName} (""{dbSchema.IdempotencyIdColumnName}"", ""{dbSchema.ValueColumnName}"") values (@idempotencyId, @nextValue)
+                    on conflict(""{dbSchema.IdempotencyIdColumnName}"") do nothing
+                    returning ""{dbSchema.ValueColumnName}""
                 )
-                select ""Value"" from new_value
+                select ""{dbSchema.ValueColumnName}"" from new_value
                 union
-                select ""Value"" from {defaultSchema}.{tableName} where ""IdempotencyId"" = @idempotencyId";
+                select ""{dbSchema.ValueColumnName}"" from {defaultSchema}.{dbSchema.TableName} where ""{dbSchema.IdempotencyIdColumnName}"" = @idempotencyId";
 
             var connection = context.Database.GetDbConnection();
 
             await using var command = connection.CreateCommand();
 
             command.CommandText = query;
-            
+
             var idempotencyParameter = command.CreateParameter();
             idempotencyParameter.ParameterName = "@idempotencyId";
             idempotencyParameter.DbType = DbType.String;
@@ -66,6 +69,40 @@ namespace Swisschain.Extensions.Idempotency.EfCore
             return reader.GetInt64(0);
         }
 
+        private static DbSchema GetDbSchema(TDbContext context)
+        {
+            // Double-checked locking
+
+            if (_dbSchemaCache != null)
+            {
+                return _dbSchemaCache;
+            }
+
+            lock (_dbSchemaCachelock)
+            {
+                if (_dbSchemaCache != null)
+                {
+                    return _dbSchemaCache;
+                }
+
+                var entityType = context.Model.FindEntityType(typeof(IdGeneratorEntity));
+                var tableName = entityType.GetTableName();
+                var schema = entityType.GetSchema();
+                var storeObjectIdentifier = StoreObjectIdentifier.Table(tableName, schema);
+                var idempotencyIdColumnName = entityType.FindProperty(nameof(IdGeneratorEntity.IdempotencyId)).GetColumnName(storeObjectIdentifier);
+                var valueColumnName = entityType.FindProperty(nameof(IdGeneratorEntity.Value)).GetColumnName(storeObjectIdentifier);
+
+                _dbSchemaCache = new DbSchema
+                {
+                    TableName = tableName,
+                    IdempotencyIdColumnName = idempotencyIdColumnName,
+                    ValueColumnName = valueColumnName
+                };
+
+                return _dbSchemaCache;
+            }
+        }
+
         private static async Task<long> GetNextValue(string defaultSchema, string generatorName, TDbContext context)
         {
             await using var cmd = context.Database.GetDbConnection().CreateCommand();
@@ -81,7 +118,14 @@ namespace Swisschain.Extensions.Idempotency.EfCore
                 cmd.Connection.Open();
             }
 
-            return (long) cmd.ExecuteScalar();
+            return (long)cmd.ExecuteScalar();
+        }
+
+        private class DbSchema
+        {
+            public string TableName { get; init; }
+            public string IdempotencyIdColumnName { get; init; }
+            public string ValueColumnName { get; init; }
         }
     }
 }
